8ffcdf8da6bd538c75c14b2aadda99f04ab5fdc3
DeltaT71 <DeltaT71@gmail.com>
2025-05-07 18:42:42 +0300

    Scafolded the home page structure and built Stories layout

---
+++ src/app/components/Stories.tsx
@@ -0,0 +1,101 @@
+import Image from "next/image";
+import React from "react";
+
+const Stories = () => {
+  return (
+    <div className="p-4 rounded-lg shadow-xl bg-neutral-700 overflow-scroll text-xs scrollbar-hide">
+      <div className="flex gap-8 w-max">
+        {/* Story */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/31716529/pexels-photo-31716529/free-photo-of-dreamy-floral-portrait-with-soft-focus.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2 ring-teal-100 object-cover"
+          />
+          <span className="font-medium">Jhon</span>
+        </div>
+        {/* Story */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/31716529/pexels-photo-31716529/free-photo-of-dreamy-floral-portrait-with-soft-focus.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2 ring-teal-100 object-cover"
+          />
+          <span className="font-medium">Jhon</span>
+        </div>
+        {/* Story */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/31716529/pexels-photo-31716529/free-photo-of-dreamy-floral-portrait-with-soft-focus.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2 ring-teal-100 object-cover"
+          />
+          <span className="font-medium">Jhon</span>
+        </div>
+        {/* Story */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/31716529/pexels-photo-31716529/free-photo-of-dreamy-floral-portrait-with-soft-focus.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2 ring-teal-100 object-cover"
+          />
+          <span className="font-medium">Jhon</span>
+        </div>
+        {/* Story */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/31716529/pexels-photo-31716529/free-photo-of-dreamy-floral-portrait-with-soft-focus.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2 ring-teal-100 object-cover"
+          />
+          <span className="font-medium">Jhon</span>
+        </div>
+        {/* Story */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/31716529/pexels-photo-31716529/free-photo-of-dreamy-floral-portrait-with-soft-focus.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2 ring-teal-100 object-cover"
+          />
+          <span className="font-medium">Jhon</span>
+        </div>
+        {/* Story */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/31716529/pexels-photo-31716529/free-photo-of-dreamy-floral-portrait-with-soft-focus.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2 ring-teal-100 object-cover"
+          />
+          <span className="font-medium">Jhon</span>
+        </div>
+        {/* Story */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/31716529/pexels-photo-31716529/free-photo-of-dreamy-floral-portrait-with-soft-focus.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2 ring-teal-100 object-cover"
+          />
+          <span className="font-medium">Jhon</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Stories;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,28 @@
+import AddPost from "./components/AddPost";
+import Feed from "./components/Feed";
+import LeftMenu from "./components/LeftMenu";
+import RightMenu from "./components/RightMenu";
+import Stories from "./components/Stories";
+
 export default function Home() {
-  return <div>Hello, time to run.</div>;
+  return (
+    <div className="flex gap-6 h-200 pt-6">
+      {/* Left Side */}
+      <div className="hidden xl:block w-[20%]">
+        <LeftMenu></LeftMenu>
+      </div>
+      {/* Center/Main Content */}
+      <div className="w-full lg:w-[70%] xl:w-[50%]">
+        <div className="flex flex-col gap-6">
+          <Stories></Stories>
+          <AddPost></AddPost>
+          <Feed></Feed>
+        </div>
+      </div>
+      {/* Right Side */}
+      <div className="hidden lg:block w-[30%]">
+        <RightMenu></RightMenu>
+      </div>
+    </div>
+  );
 }
